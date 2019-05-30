b4bc5f01864b642ecf224c9b88ce7a0a143c15bb
void.operator <void.operator.mail@gmail.com>
2019-05-30 23:39:35 +0900

    get data from dynamodb

--- handler.js
+++ handler.js
@@ -1,14 +1,29 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB({
+    region: 'ap-northeast-1'
+});
+
+module.exports.hello = (event, context) => {
+
+  const params = {
+    // テーブル名
+    "TableName": "sls_person",
+    // 条件式（カラム名＝:変数）
+    "KeyConditionExpression":"person_id = :personId",
+    // 変数の値にインプットパラメータを代入
+    "ExpressionAttributeValues": {
+      ":personId" : {"S": event.personId}
+    }
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  // クエリ実行
+  dynamo.query(params, (err, data) => {
+    console.log("dynamo_data:", data);
+    console.log("dynamo_err:", err);
+    context.done(null, data);
+  });
+
 };
